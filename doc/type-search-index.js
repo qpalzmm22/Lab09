53dd261caa8359a66bea391e8a1cf581c4a398e6
Isaac Chae <qpalzmm22@gmail.com>
2018-04-09 10:45:16 +0900

    SalesAssociate JavaDoc added

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"edu.handong.csee.java.lab09","l":"SalesAssociate"}]
--- src/main/java/edu/handong/csee/java/lab09/SalesAssociate.java
+++ src/main/java/edu/handong/csee/java/lab09/SalesAssociate.java
@@ -1,18 +1,40 @@
 package edu.handong.csee.java.lab09;
 
+/**
+ * This class implements sales associate
+ * This class only contains getters and setters of private variables of mName and mSales
+ * @author qpalzmm22
+ *
+ */
 public class SalesAssociate {
 	private String mName;
 	private double mSales;
 
+	/**
+	 * this is getter for mName
+	 * @return the member variable, mName of this object
+	 */
 	public String getName() {
 		return mName;
 	}
+	/**
+	 * this method sets member variable mName by parameter, name
+	 * @param name name of the sales associate
+	 */
 	public void setName(String name) {
 		this.mName = name;
 	}
+	/**
+	 * this method is getter for mSales
+	 * @return the member variable, mSales of this object
+	 */
 	public double getSales() {
 		return mSales;
 	}
+	/**
+	 * This method sets member variable mSales by parameter, sales
+	 * @param sales sales of the sales associate
+	 */
 	public void setSales(double sales) {
 		this.mSales = sales;
 	}
